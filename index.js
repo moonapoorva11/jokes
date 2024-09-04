0d72e22edc819ad909e8f59719b46b93070deb7f
moon123apoorva <moon2018apoorva@gmail.com>
2024-09-04 22:48:39 +0530

    joke genrator is done

---
+++ index.js
@@ -0,0 +1,17 @@
+let get = document.getElementById("get");
+
+async function myFun(){
+
+    let getting = await fetch("https://icanhazdadjoke.com/",{
+        headers :{
+                accept : "application/json"
+        }
+
+
+    })
+    let joke = await getting.json();
+    let data = joke.joke;
+    get.innerHTML=data;
+    console.log(data);
+}
+myFun()
